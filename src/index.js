99d0e7b904e5b0786246a3342c23f88647902b66
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-09-24 14:48:20 +0200

    finished core and one extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 
-console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+
+function renderPokemonCards() {
+    const container = document.querySelector('.cards');
+    container.innerHTML = '';
+
+    
+    data.forEach(pokemon => {
+        const li = document.createElement('li');
+
+        const games = pokemon.game_indices.map(g => ` ${(g.version.name).charAt(0).toUpperCase() + (g.version.name).slice(1)}`)
+
+        li.innerHTML = `
+            <li class="card card:hover">
+            <h2 class="card--title">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+            <img
+                width="256" 
+                class="card--img"
+                src=${pokemon.sprites.other["official-artwork"].front_default}
+            />
+            <ul class="card--text">
+                <li>HP: ${pokemon.stats[0].base_stat}</li>
+                <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
+                <li>DEFENSE: ${pokemon.stats[2].base_stat}</li>
+                <li>SPECIAL-ATTACK: ${pokemon.stats[3].base_stat}</li>
+                <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
+                <li>SPEED: ${pokemon.stats[5].base_stat}</li>
+
+                <li><b>POKEMON APPEARS IN:</b> ${games}</li>
+            </ul>
+            </li>   
+            `
+        container.appendChild(li);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderPokemonCards()
+})
+
